b17494617b3d8afe2b45918b45e9be225fe748bf
Paulo Henrique Nascimento de Souza <souza.phn.agro@gmail.com>
2021-07-04 09:53:50 -0400

    DIC - Balanceado

---
+++ GEXPDES/instalacao.R
@@ -0,0 +1,32 @@
+install.packages("shiny",dependencies=TRUE)
+
+install.packages("rtools",dependencies=TRUE)
+
+install.packages("shinythemes",dependencies=TRUE)
+
+install.packages("ExpDes.pt",dependencies=TRUE)
+
+install.packages("rmarkdown",dependencies=TRUE)
+
+install.packages("plotly",dependencies=TRUE)
+
+install.packages("xtable",dependencies=TRUE)
+
+install.packages("nortest",dependencies=TRUE)
+
+install.packages("car",dependencies=TRUE)
+
+install.packages("pracma",dependencies=TRUE)
+
+install.packages("stringr",dependencies=TRUE)
+
+install.packages("ds",dependencies=TRUE)
+
+install.packages("https://github.com/gExpDes/gexpdes/raw/master/GExpDes_1.0.1.zip", repos = NULL)
+
+install.packages("http://leg.ufpr.br/~walmes/pacotes/labestData_0.1.17.458.zip", repos = NULL)
+
+
+require(GExpDes)
+install.packages("http://leg.ufpr.br/~walmes/pacotes/labestData_0.1.17.458.zip", repos = NULL)
+GExpDesView()
