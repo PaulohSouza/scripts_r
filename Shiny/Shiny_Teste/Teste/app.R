9328445223930b8844e46b42f2f2c5638daa039d
Paulo Henrique Nascimento de Souza <souza.phn.agro@gmail.com>
2021-07-10 07:37:18 -0400

    desenvolvimento de interface shiny

--- Shiny/Shiny_Teste/Teste/app.R
+++ Shiny/Shiny_Teste/Teste/app.R
@@ -1,14 +1,19 @@
+library(shiny)
 library(shinydashboard)
 
-ui <- dashboardPage(
-    dashboardHeader(title = "Header da aplicacao"),
-    dashboardSidebar(),
-    dashboardBody()
-    
+ui <- fluidPage(
+    sliderInput(inputId = "number",
+                label = "Selecione numero de dados",
+                value = 500, min = 25, max = 1000),
+    plotOutput("hist")
 )
 
 server <- function(input, output){
-    
+    output$hist <- renderPlot({
+        hist(runif(input$number, min = 0, max = 1), xlab= "value",
+             main=paste(input$number, 'Dashboard - Dados selecionados'))
+    }
+    )
 }
 
 shinyApp(ui, server)
