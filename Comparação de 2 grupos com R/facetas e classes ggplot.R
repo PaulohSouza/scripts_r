266b48a26764efe68604a8d70de7ef3858448092
Paulo Henrique Nascimento de Souza <souza.phn.agro@gmail.com>
2021-09-12 13:51:30 -0400

    Comparacao de 2 grupos

--- Comparação de 2 grupos com R/facetas e classes ggplot.R
+++ Comparação de 2 grupos com R/facetas e classes ggplot.R
@@ -16,4 +16,39 @@ geom_point(mapping = aes(x = displ, y = hwy, shape = class)) + #poderia ser size
 #Facetas
 ggplot(data = mpg)+
   geom_point(mapping = aes(x = displ, y = hwy))+
-  facet_wrap(~class, nrow =2)
+  facet_wrap(~class, ncol =2)
+
+# Grid
+#Facetas
+ggplot(data = mpg)+
+  geom_point(mapping = aes(x = displ, y = hwy))+
+  facet_grid(. ~ class)
+mpg
+
+
+mpg
+
+ggplot(mpg, aes(x = displ, y = hwy))+
+  geom_point(aes(color=class))+
+  geom_smooth()
+
+ggplot(data = mpg)+
+  geom_point(mapping = aes(x = displ, y = hwy))+
+  facet_grid(. ~ class)
+
+ggplot(data = diamonds)+
+  geom_bar(
+    mapping = aes(x = cut, fill = clarity),
+    position = "dodge"
+  )
+
+seq(1,10)
+
+library(tidyverse)
+
+ggplot(data = mpg)+
+  geom_point(mapping = aes(x = displ, y = hwy))
+
+filter(mpg, cyl == 6)
+
+filter(diamonds, carat > 3)
